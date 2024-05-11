2122c9eceaaf31311b760c4d243b3bbb1cab1ae2
shermondevelopment <victorshermon@yahoo.com>
2024-05-11 16:43:42 -0300

    refactor: menu refactor

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,25 +2,35 @@ class Menu {
 
     constructor() {
         this.menuOptions = document.querySelectorAll('.aside__bottom-area')
+        this.seasonCehcked = '1'
         this.init()
     }
 
-
+    /*
+    * add click event to all menu options
+    */
     addFocusedItemMenu() {
         this.menuOptions.forEach((itemMenu) => {
             itemMenu.addEventListener('click', () => {
                this.removeItemFocudesMenu()
                itemMenu.classList.add('focused')
+               this.seasonCehcked = itemMenu.dataset.season;
             })
         })
     }
 
+    /*
+    * add click event to all menu options
+    */
     removeItemFocudesMenu() {
         this.menuOptions.forEach((itemMenu) => {
             itemMenu.classList.remove('focused')
         })
     }
 
+    /*
+    * init menu
+    */
     init() {
         this.addFocusedItemMenu()
     }
